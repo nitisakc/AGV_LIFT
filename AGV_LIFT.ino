70e9b22b0797aa7e869388351eb4f558a2016565
nitisakc <nc.north@gmail.com>
2020-07-09 17:32:31 +0700

    Update AGV_LIFT.ino

--- AGV_LIFT.ino
+++ AGV_LIFT.ino
@@ -1,6 +1,7 @@
 #include <ESP8266WiFi.h>
 #include "ESPAsyncWebServer.h"
 #include <ESP8266HTTPClient.h>
+//#include <avr/wdt.h>
 
 AsyncWebServer server(80);
 
@@ -13,6 +14,7 @@ byte PIN_UP_POS_INPUT   = D5
 int liftPos             = 0
    ,liftGo              = 0;
 bool httpSw             = false;
+int prevMillis = 0, currMillis = 0, disconn = 10;
 
 void setup() { 
   Serial.begin(9600);
@@ -23,11 +25,14 @@ void setup() {
   pinMode(PIN_DOWN_SW_INPUT, INPUT_PULLUP);
   pinMode(PIN_UP,   OUTPUT);    digitalWrite(PIN_UP   , LOW);
   pinMode(PIN_DOWN, OUTPUT);    digitalWrite(PIN_DOWN , LOW);
-  
-  WiFi.begin("AGV_AP_EXT", "1q2w3e4r");
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin("AGV_AP01", "1q2w3e4r");
+  int timeout = 30;
   while (WiFi.status() != WL_CONNECTED){
-    delay(500);
+    delay(500); 
     Serial.print(".");
+    if(timeout-- <= 0){ ESP.restart(); }
   }
   Serial.println("");
   Serial.println("WiFi connected");
@@ -56,6 +61,12 @@ void setup() {
 }
 
 void loop() {
+  currMillis = millis();
+  if ((currMillis - prevMillis) > 1000) {
+    prevMillis = currMillis;
+    if(WiFi.status() != WL_CONNECTED){ if(disconn-- <= 0){ ESP.restart(); } }  
+  }
+  
   if(digitalRead(PIN_UP_SW_INPUT) == LOW){
     liftGo = 2;     httpSw = false;
   }else if(digitalRead(PIN_DOWN_SW_INPUT) == LOW){
@@ -63,9 +74,11 @@ void loop() {
   }else if(!httpSw){   liftGo = 0;  }
   
   if(digitalRead(PIN_UP_POS_INPUT) == LOW){
-    liftPos = 2;    liftGo = 0;   httpSw = false;
+    liftPos = 2;    
+    if(liftGo == 2){ liftGo = 0;   httpSw = false; }
   }else if(digitalRead(PIN_DOWN_POS_INPUT) == LOW){
-    liftPos = 1;    liftGo = 0;   httpSw = false;
+    liftPos = 1; 
+    if(liftGo == 1){ liftGo = 0;   httpSw = false; }
   }else{  liftPos = 0; }
 
   if(liftGo == 2){      digitalWrite(PIN_UP   , HIGH); }
@@ -74,4 +87,9 @@ void loop() {
     digitalWrite(PIN_UP   , LOW);
     digitalWrite(PIN_DOWN , LOW);
   }
+  Serial.print("LIFT POS: ");
+  Serial.println(liftPos == 1 ? "DOWN" : liftPos == 2 ? "UP" : "---");
+  Serial.print("LIFT : ");
+  Serial.println(liftGo == 1 ? "DOWN" : liftGo == 2 ? "UP" : "---");
+  delay(500);
 }
